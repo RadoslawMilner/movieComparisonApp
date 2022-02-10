2f503c5098a43df52f8b251dea1a795e0022ce1a
RadoslawMilner <rmilner1@hotmail.com>
2022-02-10 17:01:25 +0100

    zaraz triple-join

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Movie.destroy_all
+Comparison.destroy_all
+
+dark_knight = Movie.create(title: "The Dark Knight", year: 2008)
+pulp_fiction = Movie.create(title: "Pulp Fiction", year: 1994)
+citizen_kane = Movie.create(title: "Citizen Kane", year: 1941)
+
+Comparison.create(superior_movie_id: dark_knight.id, inferior_movie_id: citizen_kane.id)
+Comparison.create(superior_movie_id: citizen_kane.id, inferior_movie_id: pulp_fiction.id)
